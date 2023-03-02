6c1ccc3f59f3dcfd8a37903ae5829fa90fcb300a
Anil Kuma Reddy <anilkumar1995708@gmail.com>
2023-03-02 19:58:29 +0530

    commitcommiii

---
+++ src/main/java/com/com/springboot/MySbApplication1Application.java
@@ -0,0 +1,14 @@
+package com.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySbApplication1Application {
+
+	public static void main(String[] args) {
+		System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&& ");
+		SpringApplication.run(MySbApplication1Application.class, args);
+	}
+
+}
